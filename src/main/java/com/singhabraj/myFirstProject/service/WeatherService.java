ff9c069ea333d491828b2b6685ce4635eefc096c
singhbraj <braj333singh@gmail.com>
2024-10-27 01:31:06 +0530

    use value anotation

--- src/main/java/com/singhabraj/myFirstProject/service/WeatherService.java
+++ src/main/java/com/singhabraj/myFirstProject/service/WeatherService.java
@@ -2,6 +2,7 @@ package com.singhabraj.myFirstProject.service;
 
 import com.singhabraj.myFirstProject.api.response.WeatherAPIResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -10,7 +11,8 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class WeatherService {
 
-    private static final String apiKey = "88d818d33968b2626afff0bdb3df5904";
+    @Value("${weather.api.key}")
+    private String apiKey;
     private static final String API = "http://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
     @Autowired

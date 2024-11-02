d365b79c92591621d5847a60b359ed0e0cfaae0d
singhbraj <braj333singh@gmail.com>
2024-11-03 03:21:07 +0530

    jwt init

--- src/main/java/com/singhabraj/myFirstProject/controller/PublicController.java
+++ src/main/java/com/singhabraj/myFirstProject/controller/PublicController.java
@@ -1,8 +1,14 @@
 package com.singhabraj.myFirstProject.controller;
 
 import com.singhabraj.myFirstProject.entity.User;
+import com.singhabraj.myFirstProject.service.UserDetailsServiceImpl;
 import com.singhabraj.myFirstProject.service.UserService;
+import com.singhabraj.myFirstProject.utils.JWTutil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -15,19 +21,35 @@ public class PublicController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+   private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JWTutil jwTutil;
 
     @GetMapping("/health-check")
     public String heathCheck(){
         return "Ok";
     }
 
-    @PostMapping("/create-user")
+    @PostMapping("/signup")
     public void createUser(@RequestBody User user){
         userService.saveNewUser(user);
     }
 
 
+    @PostMapping("/login")
+    public void login(@RequestBody User user) {
+         try{
+             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUserName(),user.getPassword()));
+             UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUserName());
+         }catch(Exception e){
 
+         }
+    }
 
 }
 
---
+++ src/main/java/com/singhabraj/myFirstProject/utils/JWTutil.java
@@ -0,0 +1,8 @@
+package com.singhabraj.myFirstProject.utils;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JWTutil {
+
+}
